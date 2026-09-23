a1b04f8fd67375edb0dd2e97d285dfcf12ecef62
LAI YI CHENG <111285005+ericlai0323@users.noreply.github.com>
2026-09-23 09:39:35 +0800

    Add 3-second counter to COUNTER module
    Added a new counter for 3 seconds to the COUNTER module.

--- COUNTER.v
+++ COUNTER.v
@@ -1,17 +1,20 @@
 `define COUNTS				8'd255
 `define COUNT_1SEC			10'd60
+`define COUNT_3SEC			10'd180
 `define COUNT_5SEC			10'd300
 `define COUNT_10SEC			10'd600
 
-module COUNTER(irst,iclk,ivsync,oCount_1s);
-input				irst;
-input				iclk;
-input				ivsync;
+module COUNTER(
+           input				irst,
+           input				iclk,
+           input				ivsync,
 
-output reg[7:0]		oCount_1s;
-
+           output reg[7:0]		oCount_1s,
+           output reg[7:0]     oCount_3s
+       );
 
 reg[9:0]			count_1s;
+reg[9:0]            count_3s;
 
 reg[1:0]			vsync_sr;
 wire				vs_rising;
@@ -49,7 +52,25 @@ always@(posedge iclk) begin
     end
 end
 
-
-
+// 3s
+always@(posedge iclk) begin
+    if(!irst) begin
+        oCount_3s <= 8'd0;
+        count_3s <= 10'd0;
+    end
+    else begin
+        if(vs_rising) begin
+            if(count_3s == `COUNT_3SEC - 1) begin
+                count_3s <= 10'd0;
+                if(oCount_3s == `COUNTS - 1)
+                    oCount_3s <= 8'd0;
+                else
+                    oCount_3s <= oCount_3s + 8'd1;
+            end
+            else
+                count_3s <= count_3s + 10'd1;
+        end
+    end
+end
 
 endmodule
