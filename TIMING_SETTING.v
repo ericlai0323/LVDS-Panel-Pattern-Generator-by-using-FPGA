96313212778e223df426ac551cc942b424c8a220
LAI YI CHENG <111285005+ericlai0323@users.noreply.github.com>
2026-09-23 09:42:14 +0800

    Modify timing settings and add pattern definitions
    Updated timing parameters and added new pattern definitions.

--- TIMING_SETTING.v
+++ TIMING_SETTING.v
@@ -52,16 +52,18 @@
 // VS_PERIOD = V_PIXEL + VS_BLANK(Include VS_PULSE_WIDTH)
 
 // Horizontal timing
-`define HS_PULSE_WIDTH      22'd1
+`define HS_PULSE_WIDTH      22'd120
 `define HS_PERIOD           22'd2512
 
 // Vertical timing
-`define VS_PULSE_WIDTH      22'd1
+`define VS_PULSE_WIDTH      22'd2
 `define VS_PERIOD           22'd729
 
+`define PIXEL_CLK (`HS_PERIOD * `VS_PERIOD * 60)
+
 `ifdef HV_MODE
-`define HS_BACK_PORCH   22'd296
-`define VS_BACK_PORCH   22'd2
+`define HS_BACK_PORCH   22'd200
+`define VS_BACK_PORCH   22'd4
 `endif
 
 `ifdef DE_MODE
@@ -114,22 +116,14 @@
 `define sLOCK_T28		8'd0
 `define sLOCK_T29		8'd0
 
-
-////////////////////////////////////////////////////////////////////////////////
-// Gray Step
-`define HORIZONTAL_STEP_UP      242
-`define HORIZONTAL_STEP_DOWN    1024
-`define VERTICAL_STEP_UP        68
-`define VERTICAL_STEP_DOWN      512
-
-
 ////////////////////////////////////////////////////////////////////////////////
 // Pattern & Luminance
-`define PATTERN_NUMBER      8'd23
+`define PATTERN_NUMBER      8'd27
 `define LUMIN_FIFTY         8'd186
 `define LUMIN_TWENTY        8'd128
 `define LEVEL_10            8'd10
 `define LEVEL_16            8'd16
+`define LEVEL_22            8'd22
 `define LEVEL_32            8'd32
 `define LEVEL_48            8'd48
 `define LEVEL_64            8'd64
@@ -145,73 +139,24 @@
 `define LEVEL_224           8'd224
 `define LEVEL_240           8'd240
 
+// IS CHECKER
+`define LN0                 8'd0
+`define LN1                 8'd1
+`define LN2                 8'd7
+`define LN3                 8'd15
+`define LN4                 8'd23
+`define LN5                 8'd63
+`define LN6                 8'd127
+`define LN7                 8'd191
+`define LN8                 8'd231
+`define LN9                 8'd239
+`define LN10                8'd247
+`define LN11                8'd254
+`define LN12                8'd255
 
 ////////////////////////////////////////////////////////////////////////////////
-// Aging & Special Patterns
-`define BLACK_PAT_3         8'd66
-`define BLACK_PAT_4         8'd66
-`define BLACK_PAT_5         8'd66
-`define BLACK_PAT_6         8'd66
-`define BLACK_PAT_7         8'd66
-`define PRESSURE_PAT        8'd66
-`define BLACK50Hz_PAT       8'd66
-`define LUSTER50Hz_50P      8'd66
-`define TP_5P_PAT           8'd66
-`define CHAR_H_PAT          8'd66
-`define CROSSTALK_PAT       8'd66
-`define WAKU                8'd66
-`define R2_PAT              8'd66
-`define R_PAT_L64           8'd66
-`define G_PAT_L64           8'd66
-`define B_PAT_L64           8'd66
-`define RGB_PATTERN_L64     8'd66
-`define LUSTER_20P          8'd66
-`define LUSTER_20P_1        8'd66
-`define LUSTER_20P_2        8'd66
-`define WINDOW_PAT2         8'd66
-`define VGRAY_PAT_R         8'd66
-`define VGRAY_PAT_G         8'd66
-`define VGRAY_PAT_B         8'd66
-`define HGRAY_PAT_R         8'd66
-`define HGRAY_PAT_G         8'd66
-`define HGRAY_PAT_B         8'd66
-`define VBW1_PAT            8'd66
-`define VBW_sub_PAT         8'd66
-`define VBW_sub1_PAT        8'd66
-`define FLICKER_PAT_1L2D    8'd66
-`define FLICKER_PAT_2L1D    8'd66
-`define FLICKER_PAT_2L2D    8'd66
-`define FLICKER_PAT_COLUMN  8'd66
-`define COLOR_8C            8'd66
-`define RESPONSE_PAT        8'd66
-`define LUSTER_L10          8'd66
-`define LUSTER_L16          8'd66
-`define LUSTER_L32          8'd66
-`define LUSTER_L48          8'd66
-`define LUSTER_L64          8'd66
-`define LUSTER_L80          8'd66
-`define LUSTER_L96          8'd66
-`define LUSTER_L112         8'd66
-`define LUSTER_L128         8'd66
-`define LUSTER_L144         8'd66
-`define LUSTER_L160         8'd66
-`define LUSTER_L176         8'd66
-`define LUSTER_L192         8'd66
-`define LUSTER_L208         8'd66
-`define LUSTER_L224         8'd66
-`define LUSTER_L240         8'd66
-`define R_PAT_L128          8'd66
-`define G_PAT_L128          8'd66
-`define B_PAT_L128          8'd66
-`define GB_PAT              8'd66
-`define RB_PAT              8'd66
-`define RG_PAT              8'd66
-`define B_PAT_L128          8'd66
-`define B_PAT_L128          8'd66
-`define B_PAT_L128          8'd66
-`define HBW_PAT             8'd66
-// ////////////////////////////////////////////////////////////////////////////////
-// // RA Pattern
+// RA Patterns
+////////////////////////////////////////////////////////////////////////////////
 `define BORDER_PAT          8'd0
 `define R_PAT               8'd1
 `define G_PAT               8'd2
@@ -226,7 +171,7 @@
 `define HGRAY_PAT           8'd11
 `define VBW_PAT             8'd12
 `define RGB_PAT             8'd13
-`define CHESS_PAT           8'd14  //10x10
+`define CHESS_PAT           8'd14
 `define LUSTER_50P_2        8'd15
 `define GOMI_PAT1           8'd16
 `define GOMI_PAT2           8'd17
@@ -235,5 +180,85 @@
 `define RESPONSE_PAT_5s     8'd20
 `define RESPONSE_PAT_10s    8'd21
 `define BLACK_PAT_2         8'd22
+`define IS_CHECKER_PAT      8'd23
+`define LUSTER_20P          8'd24
+`define LUSTER_L64          8'd25
+`define LUSTER_L32          8'd26
 
 
+////////////////////////////////////////////////////////////////////////////////
+// Aging & Special Patterns
+// Unused Patterns = 8'd66
+////////////////////////////////////////////////////////////////////////////////
+`define GB_PAT              8'd66
+`define RB_PAT              8'd66
+`define RG_PAT              8'd66
+
+`define BLACK_PAT_3         8'd66
+`define BLACK_PAT_4         8'd66
+`define BLACK_PAT_5         8'd66
+`define BLACK_PAT_6         8'd66
+`define BLACK_PAT_7         8'd66
+
+`define PRESSURE_PAT        8'd66
+`define BLACK50Hz_PAT       8'd66
+`define LUSTER50Hz_50P      8'd66
+`define TP_5P_PAT           8'd66
+`define CHAR_H_PAT          8'd66
+`define CROSSTALK_PAT       8'd66
+`define WAKU                8'd66
+`define R2_PAT              8'd66
+
+`define R_PAT_L64           8'd66
+`define G_PAT_L64           8'd66
+`define B_PAT_L64           8'd66
+`define RGB_PATTERN_L64     8'd66
+
+`define LUSTER_20P_1        8'd66
+`define LUSTER_20P_2        8'd66
+`define WINDOW_PAT2         8'd66
+
+`define HGRAY_PAT_R         8'd66
+`define HGRAY_PAT_G         8'd66
+`define HGRAY_PAT_B         8'd66
+
+`define VGRAY_PAT_R         8'd66
+`define VGRAY_PAT_G         8'd66
+`define VGRAY_PAT_B         8'd66
+
+`define HBW_PAT             8'd66
+`define VBW1_PAT            8'd66
+`define VBW_sub_PAT         8'd66
+`define VBW_sub1_PAT        8'd66
+
+`define COLOR_8C            8'd66
+`define RESPONSE_PAT        8'd66
+`define RESPONSE_PAT_0_5s   8'd66
+
+`define LUSTER_L10          8'd66
+`define LUSTER_L22          8'd66
+`define LUSTER_L16          8'd66
+`define LUSTER_L48          8'd66
+`define LUSTER_L80          8'd66
+`define LUSTER_L96          8'd66
+`define LUSTER_L112         8'd66
+`define LUSTER_L128         8'd66
+`define LUSTER_L144         8'd66
+`define LUSTER_L160         8'd66
+`define LUSTER_L176         8'd66
+`define LUSTER_L192         8'd66
+`define LUSTER_L208         8'd66
+`define LUSTER_L224         8'd66
+`define LUSTER_L240         8'd66
+
+`define R_PAT_L128          8'd66
+`define G_PAT_L128          8'd66
+`define B_PAT_L128          8'd66
+
+`define FLICKER_PAT_1L2D    8'd66
+`define FLICKER_PAT_2L1D    8'd66
+`define FLICKER_PAT_2L2D    8'd66
+`define FLICKER_PAT_COLUMN  8'd66
+
+`define RGBW_GRAY_PAT       8'd66
+`define CHESS_PAT_V1        8'd66
